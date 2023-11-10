155bf908dcd3840720f9831b8b5cb28b5b18cf2a
David Hanimann <d.hanimann@esri.ch>
2023-11-10 16:00:57 +0100

    Remove setAssetPath

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App/App'
-import { setAssetPath } from '@esri/calcite-components/dist/components'
-setAssetPath('https://unpkg.com/@esri/calcite-components/dist/calcite/assets')
 
 const root = createRoot(document.getElementById('root'))
 
